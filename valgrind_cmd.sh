0cd7da6ef787de0cc08b8c52807b6a43d97822a5
lolo2438 <lolo2438@hotmail.com>
2021-04-26 15:20:04 -0400

    Fixed error with valgrind file

--- valgrind_cmd.sh
+++ valgrind_cmd.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-gcc -lm -o vg.out containers/hashmap*.{c,h} constant.h
-valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --verbose --log-file=valgrind-out.txt ./vg
+gcc -lm -o vg.out containers/list*.{c,h} constant.h
+valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --verbose --log-file=valgrind-out.txt ./vg.out
